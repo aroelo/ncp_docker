97aa48012c71a7cc84b0b20b95665d33a08aa923
aniek <a.roelofs@naktuinbouw.nl>
2020-01-27 15:58:57 +0800

    Don't drop table upon restart when using dev environment.

--- services/web/entrypoint.sh
+++ services/web/entrypoint.sh
@@ -16,7 +16,7 @@ fi
 if [ "$FLASK_ENV" = "development" ]
 then
     echo "Creating the database tables..."
-    python manage.py drop_db
+#    python manage.py drop_db
     python manage.py create_db
     python manage.py seed_pavian_db
     echo "Tables created"
